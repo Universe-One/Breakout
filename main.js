7e4432e525a824c7c78eb0c5d52e29c2ad2ef72b
Universe-One <UniverseOne.Code@gmail.com>
2021-03-10 12:22:05 -0800

    Add paddle movement and confine paddle to canvas

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// TODO
+// Figure out a way around keypress delay, possibly use keypress to set variable and check if variable is set in game loop.
+// Color of brick = how many more times it has to be hit to break. Darker bricks require more hits to break
+// CONFINE PADDLE TO PLAY AREA
+
+
 // The element references have "Elem" suffixes to differentiate them from variables used in the program.
 const canvasElem = document.querySelector("#game-canvas");
 const ctx = canvasElem.getContext("2d");
@@ -5,28 +11,62 @@ const scoreElem = document.querySelector("#current-score");
 const highScoreElem = document.querySelector("#high-score");
 
 // Keyboard controls
+// The keydown event fires when a key is first pressed, and then after a delay, continuously fires if the key 
+// is held down. As a result of this delay, paddle.handleMovement() is not smooth if called directly here. 
+// To solve this issue, physically moving the paddle is done every update of the game loop and the keyboard controls 
+// simply change the paddle direction property. When a key is pressed, the paddle direction property is set to
+// the appropriate direction until that key is lifted, at which point the paddle direction property is set back to null.
 window.addEventListener("keydown", function(e) {
 	switch (e.code) {
 		case "KeyA":
 		case "ArrowLeft":
-			paddle.move("left");
+			paddle.direction = "left";
 			break;
 		case "KeyD":
 		case "ArrowRight":
-			paddle.move("right");
+			paddle.direction = "right";
+			break;
+	}
+});
+
+// When user releases direction key, set direction to null.
+window.addEventListener("keyup", function(e) {
+	switch (e.code) {
+		case "KeyA":
+		case "ArrowLeft":
+		case "KeyD":
+		case "ArrowRight":
+			paddle.direction = null;
 			break;
 	}
 });
 
 const canvas = {
 	width: canvasElem.width,
-	height: canvasElem.height
+	height: canvasElem.height,
+	clear: function() {
+		ctx.clearRect(0, 0, this.width, this.height);
+	}
 }
 
-/*const game = {
-	isOver = false
+const game = {
+	loop: function() {
+		canvas.clear();
+		paddle.draw();
+
+		// Possibly rename to "handleMovement" or rethink structure
+		paddle.handleMovement();
+
+		// Establish the game loop. window.requestAnimationFrame runs its callback function before
+		// the browser performs the next repaint. This often happens 60 times per second, but will
+		// generally match the display refresh rate in most web browsers.
+		window.requestAnimationFrame(game.loop);
+	}
 }
-*/
+
+// Enter the game loop
+window.requestAnimationFrame(game.loop);
+
 
 const paddle = {
 	// The paddle should initially be drawn at the middle of the screen. However, fillRect treats
@@ -36,27 +76,37 @@ const paddle = {
 	// to itself because both of these cause infinite recursive calls. New properties must be created,
 	// and the convention is to prepend an underscore (_) to the getter/setter name.
 	get xPos() {
-		return this._xPos || (canvas.width / 2) - (this.width / 2);
+		// Nullish coalescing operator is used instead of OR operator to handle the case when
+		// this._xPos equals 0. If the OR operator is used, the left operand evaluates to false,
+		// resetting the position of the paddle to the center of the screen. This is not the
+		// intended behavior.
+		return this._xPos ?? (canvas.width / 2) - (this.width / 2);
 	},
 	set xPos(value) {
 		this._xPos = value;
 	},
+	// width and moveSpeed values should be carefully chosen based on the value of the other, canvas.width, and
+	// the starting xPos to ensure that the paddle remains perfectly confined to the canvas (play area).
 	yPos: 360,
 	width: 80,
 	height: 16,
+	moveSpeed: 5,
+	direction: null,
 	draw: function() {
 		ctx.fillStyle = "rgb(0, 190, 255)";
 		ctx.fillRect(this.xPos, this.yPos, this.width, this.height);
 	},
-	move: function(direction) {
-		if (direction === "right") {
-			this.xPos += 1;
-			console.log(this.xPos);
-		} else {
-			this.xPos -= 1;
+	handleMovement: function() {
+		if (this.direction === "right" && this.xPos < canvas.width - this.width) {
+
+			// +/- of xPos has to be related to paddle with atm
+
+			this.xPos += this.moveSpeed;
+		} else if (this.direction === "left" && this.xPos > 0) {
+			this.xPos -= this.moveSpeed;
 		}
+		console.log(this.xPos);
 	}
 }
 
-//setInterval(paddle.draw, 400);
 paddle.draw();
